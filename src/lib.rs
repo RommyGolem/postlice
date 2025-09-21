ec6013c0cf4e416df49212e9c5faf32ed2a82922
Rom Rakharb <romr.dev@proton.me>
2025-09-21 21:19:52 +0700

    add geo data

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+use serde::Deserialize;
+use std::sync::LazyLock;
+
+#[derive(Deserialize)]
+pub struct Province {
+    pub name: String,
+    pub districts: Vec<District>,
+}
+
+#[derive(Deserialize)]
+pub struct District {
+    pub name: String,
+    pub postal_code: u32,
+    pub sub_districts: Vec<String>,
+}
+
+pub static GEO_DATA: LazyLock<Vec<Province>> = LazyLock::new(|| {
+    serde_json::from_str(include_str!("../data/geo-data.json")).unwrap_or_default()
+});
